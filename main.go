95dd2991c3d54848fccd489730e357a88c340c02
Ben Selby <benmatselby@gmail.com>
2018-04-05 22:18:51 +0100

    Validate the environment variables

--- main.go
+++ main.go
@@ -8,11 +8,46 @@ import (
 	"github.com/urfave/cli"
 )
 
+var (
+	account string
+	project string
+	team    string
+	token   string
+)
+
+// environmentVars can validate if everything is ok before we start
+func environmentVars() (bool, error) {
+	account = os.Getenv("VSTS_ACCOUNT")
+	project = os.Getenv("VSTS_PROJECT")
+	team = os.Getenv("VSTS_TEAM")
+	token = os.Getenv("VSTS_TOKEN")
+
+	if account == "" {
+		return false, fmt.Errorf("Please define VSTS_ACCOUNT as an environment variable")
+	}
+
+	if project == "" {
+		return false, fmt.Errorf("Please define VSTS_PROJECT as an environment variable")
+	}
+
+	if team == "" {
+		return false, fmt.Errorf("Please define VSTS_TEAM as an environment variable")
+	}
+
+	if token == "" {
+		return false, fmt.Errorf("Please define VSTS_TOKEN as an environment variable")
+	}
+
+	return true, nil
+}
+
 func main() {
-	account := os.Getenv("VSTS_ACCOUNT")
-	project := os.Getenv("VSTS_PROJECT")
-	team := os.Getenv("VSTS_TEAM")
-	token := os.Getenv("VSTS_TOKEN")
+
+	_, err := environmentVars()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	v := vsts.New(account, project, team, token)
 
