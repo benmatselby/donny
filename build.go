fa28bf2d36081d99c5b4e5db52a884de47fa1582
Ben Selby <benmatselby@gmail.com>
2018-04-17 22:19:29 +0100

    Print some extra information for the builds

--- build.go
+++ build.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
+	"time"
 
 	"github.com/fatih/color"
 	"github.com/urfave/cli"
@@ -14,15 +17,24 @@ func ListBuilds(c *cli.Context) {
 		fmt.Println(error)
 	}
 
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
 	for index := 0; index < len(builds); index++ {
 		name := builds[index].Definition.Name
-		// status := builds[index].Status
 		result := builds[index].Result
-
-		if result == "failed" {
-			color.Red(name)
-		} else if result == "succeeded" {
-			color.Green(name)
+		buildNo := builds[index].BuildNumber
+		finish, error := time.Parse(time.RFC3339, builds[index].FinishTime)
+		finishAt := finish.Format("2006-01-02 15:04:05")
+		if error != nil {
+			finishAt = builds[index].FinishTime
 		}
+
+		colour := color.New(color.FgGreen)
+		if result == "failed" {
+			colour = color.New(color.FgRed)
+		}
+
+		colour.Fprintf(w, "%s\t%s\t%s\t\n", name, buildNo, finishAt)
 	}
+
+	w.Flush()
 }
