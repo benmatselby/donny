ab9ebaa544396cf198caa74adf481deaed757cca
Ben Selby <benmatselby@gmail.com>
2018-04-27 22:30:38 +0100

    Deal with errors a little better

--- iteration.go
+++ iteration.go
@@ -14,7 +14,8 @@ import (
 func ListIterations(c *cli.Context) {
 	iterations, error := client.Iterations.List(team)
 	if error != nil {
-		fmt.Println(error)
+		fmt.Println(error.Error())
+		return
 	}
 
 	for index := 0; index < len(iterations); index++ {
@@ -30,12 +31,17 @@ func ListItemsInIteration(c *cli.Context) {
 
 	iterationName := c.Args()[0]
 	boardName := c.String("board")
-	items := getWorkItemsByBoardColumn(team, iterationName)
+	items, error := getWorkItemsByBoardColumn(team, iterationName)
+	if error != nil {
+		fmt.Println(error.Error())
+		return
+	}
 
 	// Get the board layout so we now how to render the columns in the right order
 	boards, error := client.Boards.List(team)
 	if error != nil {
-		fmt.Println(error)
+		fmt.Println(error.Error())
+		return
 	}
 
 	// We need to get the specific board we are interested in
@@ -68,12 +74,17 @@ func ShowIterationBurndown(c *cli.Context) {
 
 	iterationName := c.Args()[0]
 	boardName := c.String("board")
-	items := getWorkItemsByBoardColumn(team, iterationName)
+	items, error := getWorkItemsByBoardColumn(team, iterationName)
+	if error != nil {
+		fmt.Println(error.Error())
+		return
+	}
 
 	// Get the board layout so we now how to render the columns in the right order
 	boards, error := client.Boards.List(team)
 	if error != nil {
-		fmt.Println(error)
+		fmt.Println(error.Error())
+		return
 	}
 
 	// We need to get the specific board we are interested in
@@ -118,7 +129,11 @@ func ShowIterationPeopleBreakdown(c *cli.Context) {
 	}
 
 	iterationName := c.Args()[0]
-	workItems := getWorkItemsByPerson(team, iterationName)
+	workItems, error := getWorkItemsByPerson(team, iterationName)
+	if error != nil {
+		fmt.Println(error.Error())
+		return
+	}
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 1, 1, ' ', 0)
 	fmt.Fprintf(w, "%s\t%s\t%s\n", "Person", "Items", "Points")
@@ -160,8 +175,11 @@ func checkIterationDeclared(c *cli.Context) bool {
 	return true
 }
 
-func getWorkItemsByPerson(team string, iterationName string) map[string][]vsts.WorkItem {
-	workItems := getWorkItems(team, iterationName)
+func getWorkItemsByPerson(team string, iterationName string) (map[string][]vsts.WorkItem, error) {
+	workItems, error := getWorkItems(team, iterationName)
+	if error != nil {
+		return nil, error
+	}
 
 	items := make(map[string][]vsts.WorkItem)
 
@@ -173,11 +191,14 @@ func getWorkItemsByPerson(team string, iterationName string) map[string][]vsts.W
 		items[key] = append(items[key], item)
 	}
 
-	return items
+	return items, nil
 }
 
-func getWorkItemsByBoardColumn(team string, iterationName string) map[string][]vsts.WorkItem {
-	workItems := getWorkItems(team, iterationName)
+func getWorkItemsByBoardColumn(team string, iterationName string) (map[string][]vsts.WorkItem, error) {
+	workItems, error := getWorkItems(team, iterationName)
+	if error != nil {
+		return nil, error
+	}
 
 	items := make(map[string][]vsts.WorkItem)
 
@@ -193,21 +214,25 @@ func getWorkItemsByBoardColumn(team string, iterationName string) map[string][]v
 		items[key] = append(items[key], item)
 	}
 
-	return items
+	return items, nil
 }
 
-func getWorkItems(team string, iterationName string) []vsts.WorkItem {
+func getWorkItems(team string, iterationName string) ([]vsts.WorkItem, error) {
 	// Get the iteration by name
 	iteration, error := client.Iterations.GetByName(team, iterationName)
 	if error != nil {
-		fmt.Println(error)
+		return nil, error
+	}
+
+	if iteration == nil {
+		return nil, fmt.Errorf("Unable to find iteration")
 	}
 
 	// Get the items for the iteration we have found
 	workItems, error := client.WorkItems.GetForIteration(team, *iteration)
 	if error != nil {
-		fmt.Println(error)
+		return nil, error
 	}
 
-	return workItems
+	return workItems, nil
 }
