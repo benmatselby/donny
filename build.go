f773fcc046176f52c245f0bb9df08f5ed7e24303
Ben Selby <ben.selby@emisgroupplc.com>
2018-05-21 21:41:49 +0100

    Provide a overview of builds based on path for build definitions

--- build.go
+++ build.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
 	"text/tabwriter"
 	"time"
 
@@ -30,6 +32,44 @@ func ListBuilds(c *cli.Context) {
 		count = len(builds)
 	}
 
+	renderBuilds(builds, count, filterBranch)
+}
+
+// ListBuildOverview will call the VSTS API and get a list of builds for a given path
+func ListBuildOverview(c *cli.Context) {
+	filterBranch := c.String("branch")
+	path := c.String("path")
+
+	buildDefOpts := vsts.BuildDefinitionsListOptions{Path: "\\" + path}
+	definitions, err := client.BuildDefinitions.List(&buildDefOpts)
+	if err != nil {
+		fmt.Printf("unable to get a list of build definitions: %v", err)
+		return
+	}
+
+	var builds []vsts.Build
+	for _, definition := range definitions {
+		for _, branchName := range strings.Split(filterBranch, ",") {
+			build, err := getVstsBuildsForBranch(definition.ID, branchName)
+			if err != nil {
+				fmt.Printf("unable to get builds for definition %s: %v", definition.Name, err)
+			}
+			if len(build) > 0 {
+				builds = append(builds, build[0])
+			}
+		}
+	}
+
+	renderBuilds(builds, len(builds), ".*")
+}
+
+func getVstsBuildsForBranch(defID int, branchName string) ([]vsts.Build, error) {
+	buildOpts := vsts.BuildsListOptions{Definitions: strconv.Itoa(defID), Branch: "refs/heads/" + branchName, Count: 1}
+	build, err := client.Builds.List(&buildOpts)
+	return build, err
+}
+
+func renderBuilds(builds []vsts.Build, count int, filterBranch string) {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.FilterHTML)
 	fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", "", "Name", "Branch", "Build", "Finished")
 	for index := 0; index < count; index++ {
