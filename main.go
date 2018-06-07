75c87666745c47231e7588f6b52aad8dd192e69c
Ben Selby <benmatselby@gmail.com>
2018-06-07 20:41:28 +0100

    Add aliases to all the commands we have so far

--- main.go
+++ main.go
@@ -83,9 +83,10 @@ func main() {
 	app.Version = version.GITCOMMIT
 	app.Commands = []cli.Command{
 		{
-			Name:   "build:list",
-			Usage:  "    List all the builds",
-			Action: ListBuilds,
+			Name:    "build:list",
+			Usage:   "    List all the builds",
+			Action:  ListBuilds,
+			Aliases: []string{"bl"},
 			Flags: []cli.Flag{
 				cli.IntFlag{Name: "count", Value: 10, Usage: "How many builds to display"},
 				cli.StringFlag{Name: "branch", Value: ".*", Usage: "Filter by branch name"},
@@ -93,9 +94,10 @@ func main() {
 			Category: "build",
 		},
 		{
-			Name:   "build:overview",
-			Usage:  "    Show build overview for build definitions in a given path",
-			Action: ListBuildOverview,
+			Name:    "build:overview",
+			Usage:   "    Show build overview for build definitions in a given path",
+			Action:  ListBuildOverview,
+			Aliases: []string{"bo"},
 			Flags: []cli.Flag{
 				cli.StringFlag{Name: "path", Value: os.Getenv("VSTS_TEAM"), Usage: "Build definition path"},
 				cli.StringFlag{Name: "branch", Value: "master", Usage: "Filter by branch name"},
@@ -103,18 +105,20 @@ func main() {
 			Category: "build",
 		},
 		{
-			Name:   "iteration:burndown",
-			Usage:  "Show column based data for the iteration",
-			Action: ShowIterationBurndown,
+			Name:    "iteration:burndown",
+			Usage:   "Show column based data for the iteration",
+			Action:  ShowIterationBurndown,
+			Aliases: []string{"ib"},
 			Flags: []cli.Flag{
 				cli.StringFlag{Name: "board", Value: "Stories", Usage: "Display board by type"},
 			},
 			Category: "iteration",
 		},
 		{
-			Name:   "iteration:items",
-			Usage:  "List all the work items in a given iteration",
-			Action: ListItemsInIteration,
+			Name:    "iteration:items",
+			Usage:   "List all the work items in a given iteration",
+			Action:  ListItemsInIteration,
+			Aliases: []string{"ii"},
 			Flags: []cli.Flag{
 				cli.StringFlag{Name: "board", Value: "Stories", Usage: "Display board by type"},
 				cli.StringFlag{Name: "filter-tag", Value: "", Usage: "Filter by a given tag"},
@@ -126,33 +130,38 @@ func main() {
 			Name:     "iteration:list",
 			Usage:    "List all the iterations",
 			Action:   ListIterations,
+			Aliases:  []string{"il"},
 			Category: "iteration",
 		},
 		{
 			Name:     "iteration:people",
 			Usage:    "Show people based data for the iteration",
 			Action:   ShowIterationPeopleBreakdown,
+			Aliases:  []string{"ip"},
 			Category: "iteration",
 		},
 		{
 			Name:     "plan:list",
 			Usage:    "     List all the delivery plans",
 			Action:   ListDeliveryPlans,
+			Aliases:  []string{"pll"},
 			Category: "plans",
 		},
 		{
-			Name:   "plan:timeline",
-			Usage:  "     Show the timeline for the delivery plan",
-			Action: GetDeliveryPlanTimeLine,
+			Name:    "plan:timeline",
+			Usage:   "     Show the timeline for the delivery plan",
+			Action:  GetDeliveryPlanTimeLine,
+			Aliases: []string{"plt"},
 			Flags: []cli.Flag{
 				cli.BoolFlag{Name: "show-tags", Usage: "Should we show any tags for the items in the plan"},
 			},
 			Category: "plans",
 		},
 		{
-			Name:   "pr:list",
-			Usage:  "           List all the pull requests",
-			Action: ListPullRequests,
+			Name:    "pr:list",
+			Usage:   "           List all the pull requests",
+			Action:  ListPullRequests,
+			Aliases: []string{"pul"},
 			Flags: []cli.Flag{
 				cli.StringFlag{Name: "state", Value: "active", Usage: "Filter by pull request state"},
 				cli.StringFlag{Name: "repo", Value: ".*", Usage: "Filter by repo name"},
